486bf343dce34652c8364bd7cb5e1953287b5fc2
tcanham <tascanham@gmail.com>
2018-02-09 18:43:09 +0000

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,31 +1,32 @@
         <footer> 
-            <div class="footer-top">   
-                <div class="donate-button-holder footer-div">
-                    <p class="top-footer-heading">Donations</p>
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA+Kuidjg9PdeZJrSd76HpU7efrIFta9e/GZvNcjzz8XBJm9kHBJfu8DEdUI4oIUGf5WSTg5DnHhaZPZCEnwTTHcNs1bNG3gfPjJoFcHTlYe1U+lh/T90czTySSW866MvQ8tNj4qkjdL0Eyi04HRW/QFZkrhRZBnrg9H4STspW2ajELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIuBafAcLex+qAgZhATGOqdgG5aZ0w0Hp8EFgIeFA3icordzS0Z/3v2rZI+AKZ11PLjum7yHzMVTK172vzrtHcWRXdTFqVYjYeUDPyo6iFZJsXjFj7qu/MO7FZS3SH+8z3WyW0luHetPSdKVpXwhGwmOVY0HLkISU8RS5PX6CTOtgjPU0ReIOwXtOIdSzmQr/f+MzbBsu3r1NC8Rs2CmrKHr1wx6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE4MDIwNjE3MjQwMFowIwYJKoZIhvcNAQkEMRYEFP8Ej7cMYqvEaY7uOLUI3/hw6e+HMA0GCSqGSIb3DQEBAQUABIGAhNzXQDXmz407Hkb64JB1M5h+CO/QSmPZ08KBFzf0Ek4futI8PaCbxqurdl6wrwqOKbqio8Ii19OOtg86rtWU6KXd1oKxK5Gvqz4yw1i12DXxz/HEaNwa4HLnMGgJSooiP1axtzWCDjTfqh/5jwemW28YLMXcdG1ZtPeJ5DUTd9k=-----END PKCS7-----
-                    ">
-                    <input type="submit"value="Donate with Paypal"name="submit"title="PayPal - The safer, easier way to pay online!">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
-                    <a href="https://www.facebook.com/heartofirelandanimalrescue/app/117708921611213/" target="_blank"class="donate-btn">Donate with facebook</a>
-                </div> 
-                <div class="links-holder footer-div ">
-                    <p>Links</p>
-                </div>
-                <div class="spare footer-div">
-                    <p>spare</p>
-                </div> 
-                <div class="spare footer-div">
-                    <p>spare</p>
-                </div> 
-            </div>
+            <div class="footer-top"> 
+                <div class="footer_divs_outer">
+                    <div class="donate-button-holder footer-div">
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA+Kuidjg9PdeZJrSd76HpU7efrIFta9e/GZvNcjzz8XBJm9kHBJfu8DEdUI4oIUGf5WSTg5DnHhaZPZCEnwTTHcNs1bNG3gfPjJoFcHTlYe1U+lh/T90czTySSW866MvQ8tNj4qkjdL0Eyi04HRW/QFZkrhRZBnrg9H4STspW2ajELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIuBafAcLex+qAgZhATGOqdgG5aZ0w0Hp8EFgIeFA3icordzS0Z/3v2rZI+AKZ11PLjum7yHzMVTK172vzrtHcWRXdTFqVYjYeUDPyo6iFZJsXjFj7qu/MO7FZS3SH+8z3WyW0luHetPSdKVpXwhGwmOVY0HLkISU8RS5PX6CTOtgjPU0ReIOwXtOIdSzmQr/f+MzbBsu3r1NC8Rs2CmrKHr1wx6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE4MDIwNjE3MjQwMFowIwYJKoZIhvcNAQkEMRYEFP8Ej7cMYqvEaY7uOLUI3/hw6e+HMA0GCSqGSIb3DQEBAQUABIGAhNzXQDXmz407Hkb64JB1M5h+CO/QSmPZ08KBFzf0Ek4futI8PaCbxqurdl6wrwqOKbqio8Ii19OOtg86rtWU6KXd1oKxK5Gvqz4yw1i12DXxz/HEaNwa4HLnMGgJSooiP1axtzWCDjTfqh/5jwemW28YLMXcdG1ZtPeJ5DUTd9k=-----END PKCS7-----
+                            ">
+                             <button class="donate-btn" type="submit"value="Donate with Paypal"name="submit"title="PayPal - The safer, easier way to pay online!">Donate with Paypal</button>    
+                        </form>
+                            <a href="https://www.facebook.com/heartofirelandanimalrescue/app/117708921611213/" target="_blank"><button class="donate-btn " >Donate with facebook</button></a>
+                    </div> 
+                    <div class="links-holder footer-div ">
+                        <p>Links</p>
+                    </div>
+                </div>  
+                <div class="footer_divs_outer">
+                    <div class="footer-div spare"></div>
+                    <div class="footer-div">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18849.95720068698!2d-8.627697962278367!3d53.80293024811451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTPCsDQ4JzEwLjUiTiA4wrAzNiczNi43Ilc!5e0!3m2!1sen!2sie!4v1517082425840" width="100%" height="200px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    </div>
+                <div class="clearfix"></div>
+            </div>    
             <div class="footer-bottom">
                 <a href="<?= BASE_URL?>login" id="login_button" target="_blank">Login</a>    
                 <p>&copy;<?=date('Y');?>&nbsp;Heart of Ireland Animal Rescue</p>
             </div>
-            </footer>
+        </footer>
     </div>
 </body>
 </html>
